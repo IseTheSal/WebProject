dafcd402a1a0657db14f7bc5614999c050b7f798
IseTheSal <illya.ageevich@gmail.com>
2021-01-28 08:45:00 +0300

    first commit

---
+++ src/main/java/by/learning/web/model/pool/ConnectionCreator.java
@@ -0,0 +1,40 @@
+package by.learning.web.model.pool;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+class ConnectionCreator {
+
+    private static final Logger logger = LogManager.getLogger(ConnectionCreator.class);
+    private static final Properties PROPERTY = new Properties();
+    private static final String PROPERTY_PATH = "C:\\Users\\illya\\Desktop\\Epam\\Epam Learning\\Servlet\\src\\main\\resources\\database.properties";
+    private static final String DRIVER_KEY = "db.driver";
+    private static final String URL = "db.url";
+
+    static {
+        try {
+            FileReader fileReader = new FileReader(PROPERTY_PATH);
+            PROPERTY.load(fileReader);
+            String property = PROPERTY.getProperty(DRIVER_KEY);
+            Class.forName(property);
+        } catch (IOException | ClassNotFoundException e) {
+            logger.log(Level.FATAL, e);
+        }
+    }
+
+    private ConnectionCreator() {
+    }
+
+    public static Connection createConnection() throws SQLException {
+        return DriverManager.getConnection(PROPERTY.getProperty(URL), PROPERTY);
+    }
+}
---
+++ src/main/java/by/learning/web/model/pool/ConnectionPool.java
@@ -0,0 +1,63 @@
+package by.learning.web.model.pool;
+
+
+import by.learning.web.exception.ConnectionPoolException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public enum ConnectionPool {
+
+    INSTANCE;
+
+    private final Logger logger = LogManager.getLogger(ConnectionPool.class);
+
+    private static final int POOL_SIZE = 10;
+    private final BlockingQueue<ProxyConnection> freeConnections;
+    private final Queue<ProxyConnection> engagedConnections;
+
+
+    ConnectionPool() {
+        freeConnections = new LinkedBlockingQueue<>();
+        engagedConnections = new ArrayDeque<>();
+        for (int i = 0; i < POOL_SIZE; i++) {
+            try {
+                Connection connection = ConnectionCreator.createConnection();
+                ProxyConnection proxyConnection = new ProxyConnection(connection);
+                freeConnections.offer(proxyConnection);
+            } catch (SQLException exception) {
+                logger.log(Level.ERROR, exception);
+            }
+        }
+    }
+
+    public Connection getConnection() throws ConnectionPoolException {
+        ProxyConnection connection;
+        try {
+            connection = freeConnections.take();
+            engagedConnections.offer(connection);
+        } catch (InterruptedException e) {
+            logger.log(Level.FATAL, e);
+            throw new ConnectionPoolException(e);
+        }
+        return connection;
+    }
+
+
+    public void releaseConnection(ProxyConnection connection) {
+        if (engagedConnections.remove(connection)) {
+            freeConnections.offer(connection);
+            logger.log(Level.INFO, "Connection was released");
+        } else {
+            logger.log(Level.WARN, "Connection was not offered");
+        }
+    }
+
+}
