176a519135246518a63d4cac1907de383e567d38
IseTheSal <illya.ageevich@gmail.com>
2021-02-01 08:44:36 +0300

    first commit

---
+++ src/main/java/by/learning/web/controller/command/impl/HomeCommand.java
@@ -0,0 +1,42 @@
+package by.learning.web.controller.command.impl;
+
+import by.learning.web.controller.PagePath;
+import by.learning.web.controller.RequestParameter;
+import by.learning.web.controller.SessionAttribute;
+import by.learning.web.controller.command.ActionCommand;
+import by.learning.web.exception.ServiceException;
+import by.learning.web.model.entity.Game;
+import by.learning.web.model.service.GameService;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+public class HomeCommand implements ActionCommand {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    private GameService service;
+
+    public HomeCommand(GameService service) {
+        this.service = service;
+    }
+
+    @Override
+    public String execute(HttpServletRequest request) {
+        String page;
+        try {
+            List<Game> allGames = service.findAllGames();
+            HttpSession session = request.getSession();
+            session.setAttribute(SessionAttribute.GAME_LIST,allGames);
+            page = PagePath.MAIN_PAGE;
+        } catch (ServiceException e) {
+            logger.log(Level.ERROR, e);
+            page = PagePath.ERROR;
+        }
+        return page;
+    }
+}
