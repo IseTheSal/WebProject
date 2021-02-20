97ffa1e4370d3699c317c065a93bc147053ae943
IseTheSal <illya.ageevich@gmail.com>
2021-02-21 00:00:21 +0300

    0.7v

---
+++ src/main/java/by/learning/web/controller/filter/UserAccessFilter.java
@@ -0,0 +1,35 @@
+package by.learning.web.controller.filter;
+
+import by.learning.web.controller.PagePath;
+import by.learning.web.controller.RequestParameter;
+import by.learning.web.controller.SessionAttribute;
+import by.learning.web.model.entity.User;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class UserAccessFilter implements Filter {
+    private static final Logger logger = LogManager.getLogger();
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute(SessionAttribute.CURRENT_USER);
+        if (user == null) {
+            logger.log(Level.INFO, "user is null");
+            request.setAttribute(RequestParameter.NEED_AUTHORIZATION_FIRST, true);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(PagePath.LOGIN);
+            requestDispatcher.forward(req, resp);
+        } else {
+            chain.doFilter(req, resp);
+        }
+    }
+}
